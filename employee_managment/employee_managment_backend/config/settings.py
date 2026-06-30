cb6a7896cdb4dc998ad79997701d8662aa896864
yoditgirma <149189296+yoditgirma@users.noreply.github.com>
2026-06-30 05:51:14 +0300

    updated the database

--- employee_managment/employee_managment_backend/config/settings.py
+++ employee_managment/employee_managment_backend/config/settings.py
@@ -79,11 +79,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "employee_db",
         "USER": "postgres",
-        "PASSWORD": "your_password",
+        "PASSWORD": "12345678",
         "HOST": "localhost",
         "PORT": "5432",
     }
